e34c0c4ad77f70afc0eb87907aea0b43c2297c2a
fatma yamaner <fyamaner061@gmail.com>
2024-12-23 19:38:01 +0300

    applicationDbContext.cs güncellendi

--- ECommerceProject.Core/Entities/Product.cs
+++ ECommerceProject.Core/Entities/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECommerceProject.ECommerceProject.Core.Entities;
 
 namespace ECommerceProject.Core.Entities
 {
@@ -12,16 +13,31 @@ namespace ECommerceProject.Core.Entities
         public string Barcode { get; set; } // Barkod
         public decimal CostPrice { get; set; } // Maliyet Fiyatı
         public decimal SalePrice { get; set; } // Satış Fiyatı
-        public string Gender { get; set; } // Cinsiyet Açıklama
-        public string Season { get; set; } // Sezon Açıklama
-        public string Brand { get; set; } // Marka Açıklama
-        public string ProductGroup { get; set; } // Ürün Gurubu Açıklama
-        public string ProductFeature { get; set; } // Ürün Özellikler Açıklama
+        public string GenderDescription { get; set; } // Cinsiyet Açıklması
+        public string Gender { get; set; } // Cinsiyet 
+
+        public string BrandDescription { get; set; } // Marka Açıklama
+        public string BrandCode { get; set; } // Marka
+
+        public string ProductName { get; set; } // ürün Adı
+        public string ProductCode { get; set; } // ÜrünKodu
+        public string ProductGroup { get; set; } // Ürün Gurubu
+        public string ProductGroupDescription { get; set; } // Ürün Gurubu Açıklama
+        public string ProductFeature { get; set; } // Ürün Özellikler 
+        public string ProductFeatureDescription { get; set; } // Ürün Özellilkler Açıklaması+-
+        public string ColorCode { get; set; } // Renk
+        public string ColorDescription { get; set; } // Renk Açıklaması
+        public string Size { get; set; } // Beden
+        public string Inventory { get; set; } // Envanter
+        public string Kavala { get; set; } // Kavala
+
         // Relationships
         public Category Category { get; set; }
         public ICollection<OrderItem> OrderItems { get; set; }
         public ICollection<ProductDetail> ProductDetails { get; set; }
         public ICollection<Stock> Stocks { get; set; }
+        public ICollection<Season> Seasons { get; set; }
+        public ICollection<Discount> Discounts { get; set; }
         public ICollection<Review> Reviews { get; set; }
     }
 }
--- ECommerceProject.DataAccess/DbContext/ApplicationDbContext.cs
+++ ECommerceProject.DataAccess/DbContext/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ECommerceProject.Core.Entities;
+using ECommerceProject.ECommerceProject.Core.Entities;
 
 namespace ECommerceProject.DataAccess.DbContext
 {
@@ -22,11 +23,26 @@ namespace ECommerceProject.DataAccess.DbContext
             modelBuilder.Entity<Product>().Property(p => p.Barcode).HasColumnName("Barkod");
             modelBuilder.Entity<Product>().Property(p => p.CostPrice).HasColumnName("Maliyet Fiyatı");
             modelBuilder.Entity<Product>().Property(p => p.SalePrice).HasColumnName("Satış Fiyatı");
-            modelBuilder.Entity<Product>().Property(p => p.Gender).HasColumnName("Cinsiyet Açıklama");
-            modelBuilder.Entity<Product>().Property(p => p.Season).HasColumnName("Sezon Açıklama");
-            modelBuilder.Entity<Product>().Property(p => p.Brand).HasColumnName("Marka Açıklama");
-            modelBuilder.Entity<Product>().Property(p => p.ProductGroup).HasColumnName("Ürün Gurubu Açıklama");
-            modelBuilder.Entity<Product>().Property(p => p.ProductFeature).HasColumnName("Ürün Özellikler Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.GenderDescription).HasColumnName("Cinsiyet Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.ColorCode).HasColumnName("Renk Kodu");
+            modelBuilder.Entity<Product>().Property(p => p.ColorDescription).HasColumnName("Renk Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.Gender).HasColumnName("Cinsiyet");
+            modelBuilder.Entity<Season>().Property(p => p.SeasonName).HasColumnName("Sezon");
+            modelBuilder.Entity<Season>().Property(p => p.SeasonDescription).HasColumnName("Sezon Açıklama");
+
+            modelBuilder.Entity<Product>().Property(p => p.BrandDescription).HasColumnName("Marka Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.BrandCode).HasColumnName("Marka");
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).HasColumnName("Ürün Adı");
+            modelBuilder.Entity<Product>().Property(p => p.ProductCode).HasColumnName("Ürün Kodu");
+            modelBuilder.Entity<Product>().Property(p => p.ProductGroup).HasColumnName("Ürün Gurubu");
+            modelBuilder.Entity<Product>().Property(p => p.ProductGroupDescription).HasColumnName("Ürün Gurubu Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.ProductFeature).HasColumnName("Ürün Özellikler");
+            modelBuilder.Entity<Product>().Property(p => p.ProductFeatureDescription).HasColumnName("Ürün Özellikler Açıklama");
+            modelBuilder.Entity<Product>().Property(p => p.Size).HasColumnName("Beden");
+            modelBuilder.Entity<Product>().Property(p => p.Inventory).HasColumnName("Envanter");
+            modelBuilder.Entity<Product>().Property(p => p.Kavala).HasColumnName("Kavala");
+
+
 
             // Store
             modelBuilder.Entity<Store>().ToTable("Stores");
@@ -37,6 +53,12 @@ namespace ECommerceProject.DataAccess.DbContext
             modelBuilder.Entity<Warehouse>().ToTable("Warehouses");
             modelBuilder.Entity<Warehouse>().Property(w => w.WarehouseCode).HasColumnName("Depo Kodu");
             modelBuilder.Entity<Warehouse>().Property(w => w.WarehouseName).HasColumnName("Depo Adı");
+
+            //Stock
+            // modelBuilder.Entity<Stock>().Property(w => w.Id).HasColumnName("Stok Durumu");
+
+
+
         }
     }
 }
